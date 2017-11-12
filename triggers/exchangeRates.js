fd64fcff81ba5db22fea6d78c37ca264fdd4b9ea
Massimo Galbusera <max.galbusera@gmail.com>
2017-11-12 10:24:35 +0100

    import

---
+++ triggers/exchangeRates.js
@@ -0,0 +1,40 @@
+const getExchangeRates = (z, bundle) => {
+  const promise = z.request('https://api.fixer.io/latest?base='+bundle.inputData.from);
+  return promise.then((response) => {
+    let rates = response.json.rates;
+    if (bundle.inputData.to) {
+      return [{
+        id: bundle.inputData.to,
+        from: bundle.inputData.from,
+        to: bundle.inputData.to,
+        rate: rates[bundle.inputData.to],
+      }];
+    }
+    
+    return Object.keys(rates).reduce((array, currency) => {
+      array.push({
+        id: currency,
+        from: bundle.inputData.from,
+        to: currency,
+        rate: rates[currency]
+      });
+      return array;
+    }, []);
+  });
+};
+
+module.exports = {
+  key: 'exchangeRates',
+  noun: 'Exchange Rates',
+  display: {
+    label: 'Get Exchange Rates',
+    description: 'Get exchange rates'
+  },
+  operation: {
+    perform: getExchangeRates,
+    inputFields: [
+      {key: 'from', type: 'string', required: true},
+      {key: 'to', type: 'string', required: false},
+    ],
+  }
+};
